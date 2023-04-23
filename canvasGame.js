6825673c3e32795a00472310bc85dfa62f3df507
Sean <110581427+SeanAverS@users.noreply.github.com>
2023-04-23 15:55:45 -0700

    Draw squares vertically and horizontally

--- canvasGame.js
+++ canvasGame.js
@@ -2,9 +2,16 @@ let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
 let squareX = 0;
+let squareY = 0;
 
 // Create Grid with Sqaure 
 for (let row = 1; row <= 4; row++) {
-    ctx.strokeRect(squareX, 0, 100, 100);
-    squareX += 100;
+    for (let col = 1; col <= 4; col++) {
+        ctx.strokeRect(squareX, squareY, 100, 100);
+        squareX += 100;
+    }
+    // New row after inner loop
+    squareX = 0;
+    squareY += 100;
 }
+
